dc8352130d08f3625c5bf5cd706e6fc77c934bbb
Danielle Claar <danielle.claar@gmail.com>
2016-09-17 11:15:38 -0700

    updating analyses that I forgot to commit last time

---
+++ data/test2/qiime_pick_otus_KI_Platy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /macqiime/configs/bash_profile.txt
+
+pick_otus.py -i /Users/Danielle/Documents/Data_Analysis/KI_Platy/data/test2/KI14FSYM004.fasta -m uclust -s 0.97 -o /Users/Danielle/Documents/Data_Analysis/KI_Platy/data/test2/KI14FSYM004_otus_out
+
