93b7f4251b123c8f48d2270aea261e3c8bc7a42d
Danielle Claar <danielle.claar@gmail.com>
2016-11-07 14:57:27 -0800

    updating for new pipeline

--- data/rename.py
+++ data/rename.py
@@ -4,7 +4,7 @@ for root, dirs, files in os.walk("."): # Use os.walk to walk through all files a
 	for name in files:
 		#print(os.path.join(root,name))
 		s=os.path.join(root,name) # Concatenate root directory and filename into string
-		if name.endswith(".gz"): # If the file ends with .fastq ...
+		if name.endswith(".fastq"): # If the file ends with .fastq ...
 			t=s.split('/') # Split t on '/' to index individual chunks of the array
 			v=t[1].split('-') # Split v on '-' to index individual chunks of the array
 			os.rename('./fastq/'+v[0]+'.fastq') # rename file from local path + name to the name of the parent directory + .fasta
