cc7cf0bfcb72b13851de0670c1f8ff47659b06ac
daniclaar <danielle.claar@gmail.com>
2017-02-07 16:48:50 -0800

    update data\rename.py

--- data/rename.py
+++ data/rename.py
@@ -4,8 +4,9 @@ for root, dirs, files in os.walk("."): # Use os.walk to walk through all files a
 	for name in files:
 		#print(os.path.join(root,name))
 		s=os.path.join(root,name) # Concatenate root directory and filename into 
-		if name.endswith(".fastq"): # If the file ends with .fastq ...
-			t=s.split('/') # Split t on '/' to index individual chunks of the array
+		if name.endswith(".fastq.gz"): # If the file ends with .fastq.gz ...
+			t=s.split('\\') # Split t on '/' to index individual chunks of the array
+			#print(t)
 			v=t[1].split('-') # Split v on '-' to index individual chunks of the array
-			print(v)
-			#os.rename(root+'/'+name,'./fastq/'+v[0]+'.fastq') # rename file from local path + name to the name of the parent directory + .fasta
+			#print(v)
+			os.rename(root+'\\'+name,'.\\fastq\\'+v[0]+'.fastq.gz') # rename file from local path + name to the name of the parent directory + .fastq.gz
