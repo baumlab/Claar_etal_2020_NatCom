b46f9781490a7422e097b3cec6660152c9341b65
daniclaar <danielle.claar@gmail.com>
2017-06-19 10:12:38 -0700

    updating fig 1

--- figures/Figure1.R
+++ figures/Figure1.R
@@ -1,80 +1,112 @@
-# install.packages('jpeg')
-# Function from http://stackoverflow.com/questions/9543343/plot-a-jpg-image-using-base-graphics-in-r
-plot_jpeg = function(path, add=FALSE)
-{
-  require('jpeg')
-  jpg = readJPEG(path, native=T) # read the file
-  res = dim(jpg)[2:1] # get the resolution
-  if (!add) # initialize an empty plot area if add==FALSE
-    plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),asp=1,type='n',xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
-  rasterImage(jpg,1,1,res[1],res[2])
-}
-
 # Reset graphical parameters
 dev.off()
 
-tiff(file="C:/Users/Dani/Documents/Data_Analysis/KI_Platy/figures/Figure1.tiff",width = 8, height = 6,units="in",res=300)
+# Clear your environment
+rm(list=ls())
+
+# Load necessary packages
+library(imager)
+
+# Load in Data
+load("data/KI_SB_temp_DHW.RData")
+load("data/KI_satellite_heat.RData")
+
+
+tiff(file="figures/Figure1.tiff",width = 8, height = 4,units="in",res=300)
 
 # Set both inner and outer margins to 0
-par(oma=c(0,0,0,0),mar=c(3,5,1.5,1.5))
+par(oma=c(0,0,0,0),mar=c(2,2,1.5,1.5))
 # Setup layout for single top panel and 5 bottom panels
-layout(matrix(c(1,1,1,1,1,2,3,4,5,6), nrow=2, ncol=5, byrow = TRUE), heights=c(0.5,0.2))
+layout(matrix(c(1,1,1,1,1,2,3,4,5,6), nrow=2, ncol=5, byrow = TRUE), heights=c(0.5,0.3))
 # Plot top panel
-temp <- read.csv("../data/thistout_kiritimati.csv", sep=",", header=FALSE)
-mtime <- read.csv("../data/timevec_kiritimati.csv", sep=",", header=FALSE)
-dhw <- read.csv("../data/histout_kiritimati.csv", sep=",", header=FALSE)
-time <- "NA"
+# Set a start and end date for plotting
+startdate <- as.POSIXct("2014-08-01 00:00:00",tz="Pacific/Kiritimati", format="%Y-%m-%d %H:%M:%S")
+enddate <- as.POSIXct("2016-11-19 00:00:00",tz="Pacific/Kiritimati", format="%Y-%m-%d %H:%M:%S")
+# Truncate the data from startdate to enddate
+KI_heat <- KI_allsites_DHW[which(KI_allsites_DHW$xi3>startdate),]
+KI_heat <- KI_heat[which(KI_heat$xi3<enddate),]
+KI_satellite_heat <- KI_satellite_heat[which(KI_satellite_heat$time>as.Date(startdate)),]
+KI_satellite_heat <- KI_satellite_heat[which(KI_satellite_heat$time<as.Date(enddate)),]
+# Rename columns
+colnames(KI_heat)<- c("time","dhw")
 
-KI_heat <- cbind(temp,mtime,dhw,time)
-colnames(KI_heat) <- c("temp","mtime","dhw","time")
-head(KI_heat)
+cbar <- read.csv("figures/cmap_enso.csv",header=F)
+dhw.floor <- floor(KI_heat$dhw)+1
+dhw.cc <- cbar[dhw.floor,]
+dhw.cc.rgb <- rgb(dhw.cc)
+from <- KI_heat$time-1.75*86400
+to <- KI_heat$time+1.75*86400
 
-matlab2POS = function(x, timez = "UTC") {
-  days = x - 719529 	# 719529 = days from 1-1-0000 to 1-1-1970
-  secs = days * 86400 # 86400 seconds in a day
-  # This next string of functions is a complete disaster, but it works.
-  # It tries to outsmart R by converting the secs value to a POSIXct value
-  # in the UTC time zone, then converts that to a time/date string that 
-  # should lose the time zone, and then it performs a second as.POSIXct()
-  # conversion on the time/date string to get a POSIXct value in the user's 
-  # specified timezone. Time zones are a goddamned nightmare.
-  return(as.POSIXct(strftime(as.POSIXct(secs, origin = '1970-1-1', 
-                                        tz = 'UTC'), format = '%Y-%m-%d %H:%M', 
-                             tz = 'UTC', usetz = FALSE), tz = timez))
+#################################################################################
+# polycurve function
+# http://www.fromthebottomoftheheap.net/2013/01/11/shading-regions-under-a-curve/
+polyCurve <- function(x, y, from, to, n = 50, miny,
+                      col = "red", border = col) {
+  drawPoly <- function(fun, from, to, n = 50, miny, col, border) {
+    Sq <- seq(from = from, to = to, length = n)
+    polygon(x = c(Sq[1], Sq, Sq[n]),
+            y = c(miny, fun(Sq), miny),
+            col = col, border = border)
+  }
+  lf <- length(from)
+  stopifnot(identical(lf, length(to)))
+  if(length(col) != lf)
+    col <- rep(col, length.out = lf)
+  if(length(border) != lf)
+    border <- rep(border, length.out = lf)
+  if(missing(miny))
+    miny <- min(y)
+  interp <- approxfun(x = x, y = y)
+  mapply(drawPoly, from = from, to = to, col = col, border = border,
+         MoreArgs = list(fun = interp, n = n, miny = miny))
+  invisible()
 }
+#################################################################################
 
-KI_heat$time <- matlab2POS(KI_heat$mtime)
-KI_heat$time <- as.Date(KI_heat$time, '%m/%d/%Y',tz="UTC")
+# Colours for shading
+# cols <- c("yellow", "orange", "darkorange", "red", "#7d0000", "red", "darkorange", "orange", "yellow")
+cols <- c(rgb(cbar)[1], rgb(cbar)[4], rgb(cbar)[8], rgb(cbar)[12], rgb(cbar)[24])
 
-# par(mfrow=c(1,1),bg="black",fg="white",col.axis="white",col.lab="white")
-# <!-- plot(spline(KI_heat$time,KI_heat$temp), type="l", xlab="", ylab="", ylim=c(25,31), col="red") -->
-# <!-- axis(2, col="black",lwd=2) -->
-# <!-- mtext(2,text="Temperature",line=2) -->
-# par(new=TRUE)
+KI2014 <- as.POSIXct("2014-09-01 00:00:00",tz="Pacific/Kiritimati", format="%Y-%m-%d %H:%M:%S")
+KI2015a <- as.POSIXct("2015-01-25 00:00:00",tz="Pacific/Kiritimati", format="%Y-%m-%d %H:%M:%S")
+KI2015c <- as.POSIXct("2015-07-25 00:00:00",tz="Pacific/Kiritimati", format="%Y-%m-%d %H:%M:%S")
+KI2016a <- as.POSIXct("2016-03-25 00:00:00",tz="Pacific/Kiritimati", format="%Y-%m-%d %H:%M:%S")
+KI2016b <- as.POSIXct("2016-11-08 00:00:00",tz="Pacific/Kiritimati", format="%Y-%m-%d %H:%M:%S")
 
-# KI_heat$Colour <- cut(KI_heat$dhw, breaks = c(-Inf, 4, 8, 24, Inf), labels = c("yellow", "orange", "red", "maroon"))
-# KI_heat$Colour <- as.character(KI_heat$Colour)
+# Plot with the polycurve function
+with(KI_heat, plot(KI_heat$time,KI_heat$dhw, type="l", xlab="", ylab="", ylim=c(0,26),cex.axis=1,cex.lab=1.2,yaxs="i",xaxs="i",lwd=3,xaxt='n',yaxt='n',
+                   panel.first = # Panel first allows ablines to be plotted before polycurve, looks nicer.
+                     c(abline(4,0,col=cols[2],lwd=3),
+                       abline(8,0,col=cols[3],lwd=3),
+                       abline(12,0,col=cols[4],lwd=3),
+                       abline(24,0,col=cols[5],lwd=3),
+                       abline(v=KI2014,col="darkgray",lwd=2,lty=2),
+                       abline(v=KI2015a,col="darkgray",lwd=2,lty=2),
+                       abline(v=KI2015c,col="darkgray",lwd=2,lty=2),
+                       abline(v=KI2016a,col="darkgray",lwd=2,lty=2),
+                       abline(v=KI2016b,col="darkgray",lwd=2,lty=2),
+                       polyCurve(KI_heat$time, KI_heat$dhw, from = from, to = to, miny = 0,
+                                 col = dhw.cc.rgb)
+                       )))
+title(ylab="Degree Heating Weeks", line=0.5, cex.lab=1.2)
+# mtext(4,text="Degree Heating Week",line=10,cex=0.5) # Label y axis
+axis.POSIXct(side=1,KI_heat$time,cex.axis=0.93,tck=0.05,lwd.ticks=2,labels=FALSE)
+axis.POSIXct(side=1,at=seq(KI_heat$time[1],KI_heat$time[240],by="month"),KI_heat$time,tck=0.03,cex.axis=0.93,labels=c("","","Oct","","","","","","Apr","","","Jul","","","Oct","","","","","","Apr","","","Jul","","","Oct",""),lwd.ticks=1.5,padj=-1.5)
+axis.POSIXct(side=1,KI_heat$time,cex.axis=0.93,tck=0,padj=-1.5)
 
-plot(spline(KI_heat$time,KI_heat$dhw), type="l", xlab="", ylab="Degree Heating Weeks", ylim=c(0,30),cex.axis=1.5,cex.lab=2,lwd=4,xaxt='n')
-abline(4,0,col="orange")
-abline(8,0,col="darkorange")
-abline(12,0,col="red")
-abline(24,0,col="maroon")
-# ggplot(KI_heat, aes(time, dhw,color=Colour)) + geom_point() + xlab("") + ylab("Degree Heating Weeks") + scale_color_gradient(low="black",high="white", na.value = "grey50")
-
-
-# axis(4, col="black",lwd=2)
-# abline(27.897,0,col="blue",lty=2)
-mtext(4,text="Degree Heating Week",line=10,cex=1.5)
-# legend("topleft",col=c("black"),lty=c(1),lwd=4,legend=c("Heat Stress (DHW)"),box.lwd = 0,cex=1.6)
-axis.Date(side=1,KI_heat$time,cex.axis=1.5)
 par(mar=c(0.1,0.1,0.1,0.1))
 
 # Plot image panels using function created above
-plot_jpeg('../figures/coral99/KI2014_site35_99.jpg')
-plot_jpeg('../figures/coral99/KI2015a_site35_99_after.jpg')
-plot_jpeg('../figures/coral99/KI2015c_site35_99.jpg')
-plot_jpeg('../figures/coral99/KI2016a_site35_99.jpg')
-plot_jpeg('../figures/coral99/KI2016b_site35_99.jpg')
+img_KI2014_site35_99 <- load.image('figures/coral99/KI2014_site35_99.jpg')
+img_KI2015a_site35_99_after <- load.image('figures/coral99/KI2015a_site35_99_after.jpg')
+img_KI2015c_site35_99 <- load.image('figures/coral99/KI2015c_site35_99.jpg')
+img_KI2016a_site35_99 <- load.image('figures/coral99/KI2016a_site35_99.jpg')
+img_KI2016b_site35_99 <- load.image('figures/coral99/KI2016b_site35_99.jpg')
+
+plot(img_KI2014_site35_99, axes=F)
+plot(img_KI2015a_site35_99_after, axes=F)
+plot(img_KI2015c_site35_99, axes=F)
+plot(img_KI2016a_site35_99, axes=F)
+plot(img_KI2016b_site35_99, axes=F)
 
 dev.off()
