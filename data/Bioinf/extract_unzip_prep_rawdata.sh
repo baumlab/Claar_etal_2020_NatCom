afa0a64f2b61eec26330168be35d07d04df4e423
daniclaar <danielle.claar@gmail.com>
2017-07-07 16:43:39 -0700

    add how to check merge status

--- data/Bioinf/extract_unzip_prep_rawdata.sh
+++ data/Bioinf/extract_unzip_prep_rawdata.sh
@@ -6,7 +6,10 @@ find . -type f -exec gunzip {} + # Unzip these files
 python make_configs.py # Make the Illumina-utils config files for the Bokulich QC method
 
 python boku_qc.py # Run Bokulich QC method on all applicable files
-chmod u+x boku_qc_KI_Platy.sh # Give permissions so .sh file will run
+chmod u+x boku_qc_KI_Platy.
+
+
+sh # Give permissions so .sh file will run
 source ~/virtual-envs/illumina-utils-v2.0.0/bin/activate
 ./boku_qc_KI_Platy.sh # Run .sh file that was just created 
 
@@ -15,6 +18,7 @@ python make_merge_configs.py # Make the Illumina-utils config files for merging 
 python iu_merge_pairs.py # Merge pairs using Illumina-utils
 chmod u+x iu-merge-pairs_KI_Platy.sh # Give permissions so .sh file will run
 ./iu-merge-pairs_KI_Platy.sh # Run .sh file that was just created 
+# find . -maxdepth 1 -name # To check how far along the merging is (because it takes a long time to do hundreds of samples)
 
 python iu_filter_merged_reads.py # Filter merged reads (MAX-MISMATCH=3) using Illumina-utils
 
