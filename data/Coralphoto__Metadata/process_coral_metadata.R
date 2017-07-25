7a64d674c5a9eae12d443003cd5d4e1a4a61c1df
daniclaar <danielle.claar@gmail.com>
2017-07-24 19:03:49 -0700

    remove row numbering

--- data/Coralphoto__Metadata/process_coral_metadata.R
+++ data/Coralphoto__Metadata/process_coral_metadata.R
@@ -3,7 +3,7 @@ meta$ref <- paste(meta$Year_Pre_Post,".tag.",meta$coral_tag, sep="")
 meta.forcat <- meta[,c(7,10:25,27)]
 
 map <- read.table("/Users/Dani/Documents/Data_Analysis/KI_Platy/data/mapping_file.txt",stringsAsFactors = FALSE)
-colnames(map) <- c("#SampleID", "InputFileName", "coral_tag","SampleType", "Year", "TubeNumber", "Coral_Species","Site","Status","Year_Pre_Post")
+colnames(map) <- c("SampleID", "InputFileName", "coral_tag","SampleType", "Year", "TubeNumber", "Coral_Species","Site","Status","Year_Pre_Post")
 
 fs2014 <- which(map$Year==2014)
 map$Year[fs2014] <- "KI2014"
@@ -20,13 +20,14 @@ map.platy <- map
 
 names(map.platy)[names(map.platy)=="Year"] <- "field_season"
 names(map.platy)[names(map.platy)=="Site"] <- "site"
-map.platy$ref <- paste(map.platy$Year_Pre_Post,".tag.",map.platy$coral_tag, sep="")
+map.platy$ref <- paste(map.platy$Year_Pre_Post,".tag",map.platy$coral_tag, sep="")
 duplicated(map.platy)
 map.platy.forcat <- map.platy[,c(1,3:5,7:9,11)]
 
 
 metadata <- join_all(list(map.platy.forcat,meta.forcat),by='ref',match='all')
 names(metadata)
+rownames(metadata) <- metadata[,1]
 
 write.csv(metadata, file="/Users/Dani/Documents/Data_Analysis/KI_Platy/data/Coralphoto__Metadata/KI_Platy_metadata.csv")
 write.table(metadata, file="/Users/Dani/Documents/Data_Analysis/KI_Platy/data/Coralphoto__Metadata/KI_Platy_metadata.tsv", quote=FALSE, sep="\t", col.names = NA)
