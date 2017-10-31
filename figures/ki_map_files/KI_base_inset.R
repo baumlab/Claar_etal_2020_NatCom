afedd9277301d84343019e23ef20e0c5adcc03d0
Kristina Tietjen <kristinaltietjen@gmail.com>
2017-10-31 16:18:17 -0700

    working on edits to KI figure following the meeting today

---
+++ figures/ki_map_files/KI_base_inset.R
@@ -0,0 +1,43 @@
+## Script for creating KI map inset
+#setwd("/Users/jpwrobinson/Documents/git_repos/maps")
+ setwd("/Users/KristinaTietjen/Documents/Git_Hub/maps")
+# setwd("/Users/IMAC3/Documents/git-jpwrobinson/maps")
+#setwd("/Users/jamiemcdevitt-irwin/Documents/Git_Repos/maps")
+
+library(maptools)
+library(maps)
+library(scales)
+library(RColorBrewer)
+#install.packages("mapdata")
+library(mapdata)
+#install.packages("mapproj")
+library(mapproj)
+#install.packages("ggmap")
+library(ggmap)
+library(png)
+#install.packages("grImport")
+library(grImport)
+ library(rgdal)
+library(scales)
+#install.packages("PBSmapping")
+library(PBSmapping)
+
+
+
+
+
+
+
+### complex inset
+# world_shp<-readShapePoly("/Users/jpwrobinson/Documents/git_repos/maps/shapes/ne_110m_land/ne_110m_land")
+# world_shp<-readShapePoly("/Users/IMAC3/Documents/git-jpwrobinson/maps/shapes/ne_110m_land/ne_110m_land")
+ world_shp<-readShapePoly("/Users/KristinaTietjen/Documents/Git_Hub/maps/shapes/ne_110m_land/ne_110m_land")
+#world_shp <- readShapePoly("/Users/jamiemcdevitt-irwin/Documents/Git_Repos/maps/shapes/ne_110m_land/ne_110m_land")
+
+plot(world_shp, xlim=c(-180,-80),ylim=c(-40,40), col=alpha("#B6A178", 0.3), bg="white",cex=0.2, lwd=0.2)
+box(lwd=1)
+axis(1,lwd=0.2, at=c(-180, -140, -100),cex.axis=0.8, labels=c(expression(paste(180, degree, "W")), expression(paste(140, degree, "W")), expression(paste(100, degree, "W"))))
+axis(2, las=2,lwd=0.2, at=c(-40, 0, 40), cex.axis=0.8, labels=c(expression(paste(40, degree, "S")), expression(paste(0, degree)), expression(paste(40, degree, "N"))))
+#rect(-158, 1, -156, 3, border="red", lwd=2)
+points(-157.4, 1.9, col=1, pch=2, cex=1.5)
+
