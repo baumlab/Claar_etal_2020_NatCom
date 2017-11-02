c6c580cc12ff1c94e5d6fb52ea5d6f4e805d87cc
Kristina Tietjen <kristinaltietjen@gmail.com>
2017-11-02 16:00:36 -0700

    adjusted everything after making the axis text bigger

--- figures/ki_map_files/KI_base_B&W_bigger.R
+++ figures/ki_map_files/KI_base_B&W_bigger.R
@@ -29,12 +29,14 @@ mat[60:95, 10:40]<-2
 mat
 
 layout(mat)
-par(mar=c(5,8,0.3,0.3))
-plot(diva_shp, xlim=c(-157.57, -157.15), ylim=c(1.73, 2.01), col=land, border=alpha("black",0.5), lwd=1)
+par(mar=c(5,5.1,0.3,0.3))
+plot(diva_shp, xlim=c(-157.59, -157.15), ylim=c(1.73, 2.01), col=land, border=alpha("black",0.5), lwd=1)
 par(cex=2.5)
 #par(ps=35)
-axis(1, labels=c(expression(paste(157.6, degree, "W")),expression(paste(157.5, degree, "W")), expression(paste(157.4, degree, "W")), expression(paste(157.3, degree, "W")), expression(paste(157.2, degree, "W")), expression(paste(157.1, degree, "W"))), 
-	at=c(-157.6,-157.5, -157.4, -157.3,  -157.2, -157.1))
+#axis(1, padj = 0.8, labels=c(expression(paste(157.6, degree, "W")),expression(paste(157.5, degree, "W")), expression(paste(157.4, degree, "W")), expression(paste(157.3, degree, "W")), expression(paste(157.2, degree, "W")), expression(paste(157.1, degree, "W"))), 
+#	at=c(-157.6,-157.5, -157.4, -157.3,  -157.2, -157.1))
+axis(1, padj = 0.8, labels=c(expression(paste(157.6, degree, "W")),expression(paste(157.4, degree, "W")), expression(paste(157.2, degree, "W"))), 
+     at=c(-157.6, -157.4,  -157.2))
 par(cex=2.5)
 #par(ps=35)
 axis(2, at=c(1.6, 1.7, 1.8, 1.9, 2, 2.1), las=0, labels=c(expression(paste(1.6, degree, "N")),expression(paste(1.7, degree, "N")),expression(paste(1.8, degree, "N")), expression(paste(1.9, degree, "N")),expression(paste(2, degree, "N")),
@@ -44,14 +46,14 @@ box(lwd=1)
 # # scale bar. for distances at the equator, where 0.1 degree of longitude = 11.132 km 
 
 ### at bottom of plot
-rect(-157.3,1.65,  -157.202, 1.65, col="black")  ###  EQUAL TO 10 KM
-rect(-157.3, 1.65, -157.3, 1.66)
-rect(-157.202, 1.65, -157.202, 1.66)
-rect(-157.251, 1.65, -157.251, 1.66)
+rect(-157.29,1.65,  -157.192, 1.65, col="black")  ###  EQUAL TO 10 KM
+rect(-157.29, 1.65, -157.29, 1.66)
+rect(-157.192, 1.65, -157.192, 1.66)
+rect(-157.241, 1.65, -157.241, 1.66)
 # rect(-157.3102, 1.69, -157.3102, 1.691)
-text(-157.3, 1.67, "0km", cex=0.7, font=1)
-text(-157.251, 1.67, "5km", cex=0.7, font=1)
-text(-157.202, 1.67, "10km", cex=0.7, font=1)
+text(-157.29, 1.67, "0km", cex=0.7, font=1)
+text(-157.241, 1.67, "5km", cex=0.7, font=1)
+text(-157.192, 1.67, "10km", cex=0.7, font=1)
 # text(-157.3102, 1.72, "20 km", cex=0.8, font=1)
 
 # ## at top of plot
--- figures/ki_map_files/KI_maps_platyms.R
+++ figures/ki_map_files/KI_maps_platyms.R
@@ -85,15 +85,15 @@ dev.off()
 villages<-read.csv("ki_map_files/KI_villagesDCC_2015update.csv", header = TRUE) # you get an error but it works
 
 villages<-data.frame(c(1.989386333, 2.022090868, 1.983594483, 1.865048333, 1.665, 1.691, 1.715))
-villages$lon<-c(-157.4760637, -157.4884092, -157.3683462, -157.5522183, -157.57, -157.57, -157.57)
+villages$lon<-c(-157.4760637, -157.4884092, -157.3683462, -157.5522183, -157.589, -157.589, -157.589)
 villages$pop<-c(1879, 2311, 955, 441, 1500, 1000, 500)
 villages$village<-c("London", "Tabwakea", "Banana", "Poland", "legend1500", "legend1000", "legend500")
 colnames(villages)[1]<-"lat"
 
 setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy/figures")
 #tiff(file="KI_map_platysites_villages_bigger.tiff",width = 7.6, height = 7.2,units="in",res=300)
-#jpeg(file="KI_map_platysites_villages_bigger.jpeg",width = 7.6, height = 7.2,units="in",res=300)
-pdf(file="KI_map_platysites_villages_bigger.pdf", width = 7.5, height =7)
+jpeg(file="KI_map_platysites_villages_bigger.jpeg",width = 7.6, height = 7.2,units="in",res=300)
+#pdf(file="KI_map_platysites_villages_bigger.pdf", width = 7.5, height =7)
 source("ki_map_files/KI_base_B&W_bigger.R")
 
 # village markers sized by population
@@ -103,13 +103,13 @@ text(-157.4475, 1.663, "Village with 1500 people", cex=0.8)
 text(-157.4475, 1.6885, "Village with 1000 people", cex=0.8)   
 text(-157.453, 1.714, "Village with 500 people", cex=0.8)   
 #with(villages[!villages$village=="legend",], text(lon, lat, label=village, pos=1, offset=0.5, font=2, col="black"))
-points(sites$lon, sites$lat, bg=alpha(sites$col,0.8), pch=21, cex=1.8)
+points(sites$lon, sites$lat, bg=alpha(sites$col,0.8), pch=21, cex=1.4) 
 #with(sites_platy, text(lon, lat, label=site, cex=0.7))
-legend(-157.265, 2.08,legend=levels(sites$f.pressure), pt.bg=c("#8c510a","#d8b365","#c7eae5","#5ab4ac","#01665e"), pch=21, bty="n", pt.cex=1.8, cex=0.7)
-text(-157.275, 2.01, "Human disturbance", srt=90, cex=0.7)
-segments(-157.26, 1.957,-157.26, 2.056)
-text(-157.3, 1.88, "Bay of\nWrecks", cex = 0.6)
-text(-157.53, 1.82, "Vaskess\nBay", cex = 0.6)
+legend(-157.265, 2.075,legend=levels(sites$f.pressure), pt.bg=c("#8c510a","#d8b365","#c7eae5","#5ab4ac","#01665e"), pch=21, bty="n", pt.cex=1.4, cex=0.6)
+text(-157.275, 2.01, "Human disturbance", srt=90, cex=0.6)
+segments(-157.263, 1.968,-157.263, 2.057)
+text(-157.3, 1.88, "Bay of\nWrecks", cex = 0.5)
+text(-157.53, 1.82, "Vaskess\nBay", cex = 0.5)
 #source("ki_map_files/KI_base_inset.R")
 
 dev.off()
