e2150c9b655e0e51cd8fb62ebfba0118a9092f4c
Kristina Tietjen <kristinaltietjen@gmail.com>
2017-11-02 17:30:48 -0700

    put the inset in the figure with larger text

--- figures/ki_map_files/KI_base_inset_forbigger.R
+++ figures/ki_map_files/KI_base_inset_forbigger.R
@@ -36,9 +36,9 @@ library(PBSmapping)
 
 plot(world_shp, xlim=c(-180,-80),ylim=c(-40,40), col=alpha("#B6A178", 0.3), bg="white",cex=0.2, lwd=0.2)
 box(lwd=1)
-par(cex=0.8)
-axis(1,lwd=0.2, at=c(-180, -140, -100),cex.axis=0.8, labels=c(expression(paste(180, degree, "W")), expression(paste(140, degree, "W")), expression(paste(100, degree, "W"))))
-axis(2, las=2,lwd=0.2, at=c(-40, 0, 40), cex.axis=0.8, labels=c(expression(paste(40, degree, "S")), expression(paste(0, degree)), expression(paste(40, degree, "N"))))
+
+axis(1,lwd=0.2,padj = -4.2, at=c(-180, -100),cex.axis=0.5, tck=-0.05, labels=c(expression(paste(180, degree, "W")), expression(paste(100, degree, "W"))))
+axis(2, las=0,lwd=0.2,padj = 3.2, at=c(-40, 0, 40), cex.axis=0.5, tck=-0.05, labels=c(expression(paste(40, degree, "S")), expression(paste(0, degree)), expression(paste(40, degree, "N"))))
 #rect(-158, 1, -156, 3, border="red", lwd=2)
 points(-157.4, 1.9, col=1, pch=2, cex=1.5)
 
--- figures/ki_map_files/KI_maps_platyms.R
+++ figures/ki_map_files/KI_maps_platyms.R
@@ -7,6 +7,7 @@ library(maptools)
 library(scales)
 library(RColorBrewer)
 library(rgdal)
+library(ggplot2)
 
 ### site data
 sites<-read.csv('ki_map_files/ki_sites_platy.csv')
@@ -40,7 +41,7 @@ sites$col<-fishing.cols$fishing.cols[match(sites$f.pressure, fishing.cols$f.pres
 setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy/figures")
 #pdf(file="KI_map_sites_platyms.pdf")
 #tiff(file="KI_map_sites_platyms.tiff",width = 7, height = 7,units="in",res=300)
-jpeg(file="KI_map_sites_platyms.jpeg",width = 7, height = 7,units="in",res=300)
+#jpeg(file="KI_map_sites_platyms.jpeg",width = 7, height = 7,units="in",res=300)
 source("ki_map_files/KI_base_B&W.R")
 points(sites$lon, sites$lat, bg=alpha(sites$col,0.7), pch=21, cex=3.0)
 #with(sites_platy, text(lon, lat, label=site, cex=0.7))
@@ -92,7 +93,7 @@ colnames(villages)[1]<-"lat"
 
 setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy/figures")
 #tiff(file="KI_map_platysites_villages_bigger.tiff",width = 7.6, height = 7.2,units="in",res=300)
-jpeg(file="KI_map_platysites_villages_bigger.jpeg",width = 7.6, height = 7.2,units="in",res=300)
+#jpeg(file="KI_map_platysites_villages_bigger.jpeg",width = 7.6, height = 7.2,units="in",res=300)
 #pdf(file="KI_map_platysites_villages_bigger.pdf", width = 7.5, height =7)
 source("ki_map_files/KI_base_B&W_bigger.R")
 
@@ -156,3 +157,46 @@ par(mar=c(3,4,3,0))
 source("ki_map_files/KI_base_inset.R")
 
 dev.off()
+
+
+
+
+
+############### same thing as before but making the text bigger ####################
+
+setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy/figures")
+###village data
+villages<-read.csv("ki_map_files/KI_villagesDCC_2015update.csv", header = TRUE) # you get an error but it works
+
+villages<-data.frame(c(1.989386333, 2.022090868, 1.983594483, 1.865048333, 1.852, 1.8791, 1.9045))
+villages$lon<-c(-157.4760637, -157.4884092, -157.3683462, -157.5522183, -157.263, -157.263, -157.263)
+villages$pop<-c(1879, 2311, 955, 441, 1500, 1000, 500)
+villages$village<-c("London", "Tabwakea", "Banana", "Poland", "legend1500", "legend1000", "legend500")
+colnames(villages)[1]<-"lat"
+
+setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy/figures")
+#tiff(file="KI_map_platysites_villages_inset_bigger.tiff",width = 7.6, height = 7.2,units="in",res=300)
+jpeg(file="KI_map_platysites_villages_inset_bigger.jpeg",width = 7.5, height = 7,units="in",res=300)
+#pdf(file="KI_map_platysites_villages_inset_bigger.pdf", width = 7.5, height =7)
+source("ki_map_files/KI_base_B&W_bigger.R")
+
+# village markers sized by population
+symbols(villages$lon, villages$lat, circles=(villages$pop)/10, add=TRUE,inches=0.3, bg=alpha("red", 0.4))
+## legend for village size
+text(-157.19, 1.85, "1500 people", cex=0.66)   
+text(-157.19, 1.877, "1000 people", cex=0.66)   
+text(-157.1945, 1.904, "500 people", cex=0.66)  
+#with(villages[!villages$village=="legend",], text(lon, lat, label=village, pos=1, offset=0.5, font=2, col="black"))
+points(sites$lon, sites$lat, bg=alpha(sites$col,0.8), pch=21, cex=1.4) 
+#with(sites_platy, text(lon, lat, label=site, cex=0.7))
+legend(-157.265, 2.075,legend=levels(sites$f.pressure), pt.bg=c("#8c510a","#d8b365","#c7eae5","#5ab4ac","#01665e"), pch=21, bty="n", pt.cex=1.4, cex=0.6)
+text(-157.275, 2.01, "Human disturbance", srt=90, cex=0.6)
+segments(-157.263, 1.968,-157.263, 2.057)
+text(-157.315, 1.88, "Bay of\nWrecks", cex = 0.45)
+text(-157.524, 1.825, "Vaskess\nBay", cex = 0.45)
+par(mar=c(1.3,0.9,0.5,0.5))
+source("ki_map_files/KI_base_inset_forbigger.R")
+
+dev.off()
+
+
