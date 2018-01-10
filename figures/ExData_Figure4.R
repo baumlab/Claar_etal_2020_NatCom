10b017d2fb1622fc8c61eeddefd372c6e2abcc73
daniclaar <danielle.claar@gmail.com>
2018-01-09 19:00:35 -0800

    extended data

--- figures/ExData_Figure4.R
+++ figures/ExData_Figure4.R
@@ -34,13 +34,16 @@ site40_col <- "#ccebc5"
 #ffed6f
   
 ## daily trend for each site on one graph for El Nino
-p <- ggplot(site15_1hr, aes(xi2,temperature_1hr)) + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+p <- ggplot(site15_1hr, aes(x=xi2,temperature_1hr)) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.text = element_text(size=10),
+        legend.position = "top") +
   scale_x_datetime(limits=c(startday,endday),expand=c(0,0),date_breaks = "1 month", date_labels = "%b-%Y") +
   labs(x="") + 
-  scale_y_continuous(limits=c(24.5,31),breaks = c(25,26,27,28,29,30,31),expand=c(0,0),name="Temperature (Â°C)") +
-  geom_line(col=site15_col) + 
-  geom_line(data=site3_1hr,col=site3_col) + 
+  scale_y_continuous(limits=c(24.5,31),breaks = c(25,26,27,28,29,30,31),expand=c(0,0),name="Temperature (°C)") +
+  geom_line(col=site15_col,show.legend = TRUE) + 
+  geom_line(data=site3_1hr,col=site3_col,show.legend = TRUE) + 
   geom_line(data=site5_1hr,col=site5_col) + 
   geom_line(data=site8_1hr,col=site8_col) +
   geom_line(data=site25_1hr,col=site25_col) +
@@ -51,7 +54,8 @@ p <- ggplot(site15_1hr, aes(xi2,temperature_1hr)) +
   geom_line(data=site35_1hr,col=site35_col) +
   geom_line(data=site40_1hr,col=site40_col) +
   geom_abline(slope=0,intercept=28.1,linetype="dashed")+
-  geom_abline(slope=0,intercept=29.1,color="maroon")
+  geom_abline(slope=0,intercept=29.1,color="maroon")+
+  scale_color_manual(values = c(site15_col,site3_col,site5_col,site8_col,site25_col,site27_col, site30_col,site32_col,site34_col,site35_col,site40_col))
 
 jpeg(file="figures/Extended Data/ExData_Figure4.jpeg", height=8, width=15, units = "in",res = 300)
 p
