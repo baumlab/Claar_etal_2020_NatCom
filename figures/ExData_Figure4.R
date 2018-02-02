b7c44c21c6d85c18979ea0b927d829093ccd23b4
Kristina Tietjen <kristinaltietjen@gmail.com>
2018-02-01 16:55:20 -0800

    changed the code so no need to set working directory at the begining of the code - except for in the map code since the source() code changes it so you have to change it back - this meant I created a new rscript for the creation of the map so the change wd is in only one file

--- figures/ExData_Figure4.R
+++ figures/ExData_Figure4.R
@@ -1,8 +1,6 @@
 #### Making figures for the Platy manuscript #####
 ##### Beginning code copied from site.comparisons.R #####
 
-#set wd
-setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy")
 
 rm(list=ls())
 
@@ -107,58 +105,9 @@ dev.off()
 # 
 
 
-############create a map to be an inset ######################
 
-#set wd
-setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy/figures")
+############## Inset the map (from different R script) into this plot ##########################
 
-### site data
-sites<-read.csv('ki_map_files/ki_sites_platy.csv')
-
-#take out sites that did not have a seabird at them
-sites<-sites[!sites$site=="14",]
-sites<-sites[!sites$site=="37",]
-sites<-sites[!sites$site=="38",]
-
-## set palette 
-
-# `Site 3` <- "#8dd3c7"
-# `Site 5` <- "#ffffb3"
-# `Site 8` <- "#bebada"
-# `Site 15` <-  "#fb8072"
-# `Site 25` <- "#80b1d3"
-# `Site 27` <- "#fdb462"
-# `Site 30` <-  "#b3de69"
-# `Site 32` <- "#fccde5"
-# `Site 34` <- "#d9d9d9"
-# `Site 35` <- "#bc80bd"
-# `Site 40` <- "#ccebc5"
-
-psites<-c("3","5","8","15","25", "27", "30","32", "34", "35", "40")
-site.cols<-as.data.frame(psites)
-site.cols$col<-c("#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5")
-sites$col<-site.cols$col[match(sites$site, site.cols$psites)]
-
-
-### Sites sampled for platy paper
-#pdf(file="KI_map_sites_temp_platyms.pdf")
-#png(file="KI_map_sites_temp_platyms.png",width = 7, height = 7,units="in",res=300)
-#tiff(file="KI_map_sites_temp_platyms.tiff",width = 7, height = 7,units="in",res=300)
-jpeg(file="KI_map_sites_temp_platyms.jpeg",width = 7, height = 7,units="in",res=300)
-source("ki_map_files/KI_base_B&W.R")
-points(sites$lon, sites$lat, bg=alpha(sites$col,0.9), pch=21, cex=3.0)
-with(sites, text(lon, lat, label=site, cex=0.7))
-text(-157.3, 1.9, "Bay\n of\n Wrecks", cex=1)
-text(-157.52, 1.82, "Vaskess\n Bay", cex=1)
-
-dev.off()
-
-
-
-############## now going to inset it to the figure ##########################
-
-#set wd
-setwd("/Users/KristinaTietjen/Documents/Git_Hub/KI_Platy/figures/Extended_Data")
 
 require(grImport)
 library(png)
@@ -169,10 +118,10 @@ library(here)
 
 
 #read in the plot
-plot<-image_read("ExData_Figure4.png")
+plot<-image_read("figures/Extended_Data/ExData_Figure4.png")
 
 #read in the map
-map_raw<-image_read("KI_map_sites_temp_platyms.png")
+map_raw<-image_read("figures/KI_map_sites_temp_platyms.png")
 
 #scale the map and add the Kiritimati Island text
 map<- map_raw%>%
@@ -186,7 +135,7 @@ final_plot<-image_apply(map, function(x){image_composite(plot, x, offset = "+252
 #final_plot
 
 #save file
-image_write(final_plot, path = "ExData_Figure4_2.png", format = "png")
-image_write(final_plot, path = "ExData_Figure4_2.jpeg", format = "jpeg")
-image_write(final_plot, path = "Figure_S2.tiff", format = "tiff")
+image_write(final_plot, path = "figures/Extended_Data/ExData_Figure4_2.png", format = "png")
+image_write(final_plot, path = "figures/Extended_Data/ExData_Figure4_2.jpeg", format = "jpeg")
+image_write(final_plot, path = "figures/Extended_Data/Figure_S2.tiff", format = "tiff")
  
