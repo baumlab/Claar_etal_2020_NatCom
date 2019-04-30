cf0c2f14a4b1813cb4ab7aba84509f03952a528e
daniclaar <danielle.claar@gmail.com>
2019-04-29 18:50:36 -0700

    make basic color script and RData, will need to update

---
+++ figures/KI_Platy_set_colors.R
@@ -0,0 +1,10 @@
+##################################
+rm(list=ls())
+C_col <- "#2166ac"
+D_col <- "#b2182b"
+stress_col <- "#d95f02"
+# stress_col <- "darkgoldenrod4"
+# stress_col <- "burlywood4"
+stress_col <- "#ffcc66"
+
+save.image("figures/KI_Platy_colors.RData")
