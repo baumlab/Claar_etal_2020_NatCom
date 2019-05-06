c655239ab6783765afcd582acb72e388b8f4412b
daniclaar <danielle.claar@gmail.com>
2019-05-06 14:29:06 -0700

    remove 2016a samples from barplots

--- figures/barplot_symportal.R
+++ figures/barplot_symportal.R
@@ -11,32 +11,38 @@ phy97.f.c.coral.AD.before.p <- transform_sample_counts(phy97.f.c.coral.AD.before
 clade_cols <- c(A="yellow",C="blue",D="red",B="orange")
 phy97.f.c.platy.AD.before.p <- transform_sample_counts(phy97.f.c.platy.AD.before, function(x) x/sum(x))
 phy97.f.c.platy.AD.before.p.no2015c <- subset_samples(phy97.f.c.platy.AD.before.p,field_season!="KI2015c")
+phy97.f.c.platy.AD.before.p.no2015c <- subset_samples(phy97.f.c.platy.AD.before.p.no2015c,field_season!="KI2016a")
 plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 
 phy97.f.c.fpenta.AD.before.p <- transform_sample_counts(phy97.f.c.fpenta.AD.before, function(x) x/sum(x))
 phy97.f.c.fpenta.AD.before.p.no2015c <- subset_samples(phy97.f.c.fpenta.AD.before.p,field_season!="KI2015c")
+phy97.f.c.fpenta.AD.before.p.no2015c <- subset_samples(phy97.f.c.fpenta.AD.before.p.no2015c,field_season!="KI2016a")
 plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 
 
 phy97.f.c.dipmat.AD.before.p <- transform_sample_counts(phy97.f.c.dipmat.AD.before, function(x) x/sum(x))
 phy97.f.c.dipmat.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipmat.AD.before.p,field_season!="KI2015c")
+phy97.f.c.dipmat.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipmat.AD.before.p.no2015c,field_season!="KI2016a")
 plot_bar(phy97.f.c.dipmat.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 plot_bar(phy97.f.c.dipmat.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 
 phy97.f.c.dipsp.AD.before.p <- transform_sample_counts(phy97.f.c.dipsp.AD.before, function(x) x/sum(x))
 phy97.f.c.dipsp.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipsp.AD.before.p,field_season!="KI2015c")
+phy97.f.c.dipsp.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipsp.AD.before.p.no2015c,field_season!="KI2016a")
 plot_bar(phy97.f.c.dipsp.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 plot_bar(phy97.f.c.dipsp.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 
 phy97.f.c.hydno.AD.before.p <- transform_sample_counts(phy97.f.c.hydno.AD.before, function(x) x/sum(x))
 phy97.f.c.hydno.AD.before.p.no2015c <- subset_samples(phy97.f.c.hydno.AD.before.p,field_season!="KI2015c")
+phy97.f.c.hydno.AD.before.p.no2015c <- subset_samples(phy97.f.c.hydno.AD.before.p.no2015c,field_season!="KI2016a")
 plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 
 phy97.f.c.plob.AD.before.p <- transform_sample_counts(phy97.f.c.plob.AD.before, function(x) x/sum(x))
 phy97.f.c.plob.AD.before.p.no2015c <- subset_samples(phy97.f.c.plob.AD.before.p,field_season!="KI2015c")
+phy97.f.c.plob.AD.before.p.no2015c <- subset_samples(phy97.f.c.plob.AD.before.p.no2015c,field_season!="KI2016a")
 plot_bar(phy97.f.c.plob.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 plot_bar(phy97.f.c.plob.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
 
@@ -124,3 +130,4 @@ p2 <- plot_bar(phy97.f.c.dipmat.AD.before.p.no2015c,fill="majority_its2_sequence
 grid.arrange(p1,p2,nrow=2)
 
 dev.off()
+
