735feb3be444e875e9ecc150f889d4a425774ea5
daniclaar <danielle.claar@gmail.com>
2019-05-24 12:21:09 -0700

    make barplots for supplementary materials

---
+++ figures/barplot_symportal_suppmat.R
@@ -0,0 +1,193 @@
+rm(list=ls())
+
+library(phyloseq)
+library(gridExtra)
+library(ggplot2)
+library(RColorBrewer)
+
+load(file = "data/KI_seqs_f_coral_grouped_symportal.RData")
+
+phy97.f.c.coral.AD.before.p <- transform_sample_counts(phy97.f.c.coral.AD.before, function(x) x/sum(x))
+
+clade_cols <- c(A="yellow",C="blue",D="red",B="orange")
+phy97.f.c.platy.AD.before.p <- transform_sample_counts(phy97.f.c.platy.AD.before, function(x) x/sum(x))
+phy97.f.c.platy.AD.before.p.no2015c <- subset_samples(phy97.f.c.platy.AD.before.p,field_season!="KI2015c")
+phy97.f.c.platy.AD.before.p.no2015c <- subset_samples(phy97.f.c.platy.AD.before.p.no2015c,field_season!="KI2016a")
+plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+
+phy97.f.c.fpenta.AD.before.p <- transform_sample_counts(phy97.f.c.fpenta.AD.before, function(x) x/sum(x))
+phy97.f.c.fpenta.AD.before.p.no2015c <- subset_samples(phy97.f.c.fpenta.AD.before.p,field_season!="KI2015c")
+phy97.f.c.fpenta.AD.before.p.no2015c <- subset_samples(phy97.f.c.fpenta.AD.before.p.no2015c,field_season!="KI2016a")
+plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+
+
+# phy97.f.c.dipmat.AD.before.p <- transform_sample_counts(phy97.f.c.dipmat.AD.before, function(x) x/sum(x))
+# phy97.f.c.dipmat.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipmat.AD.before.p,field_season!="KI2015c")
+# phy97.f.c.dipmat.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipmat.AD.before.p.no2015c,field_season!="KI2016a")
+# plot_bar(phy97.f.c.dipmat.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+# plot_bar(phy97.f.c.dipmat.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+# 
+# phy97.f.c.dipsp.AD.before.p <- transform_sample_counts(phy97.f.c.dipsp.AD.before, function(x) x/sum(x))
+# phy97.f.c.dipsp.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipsp.AD.before.p,field_season!="KI2015c")
+# phy97.f.c.dipsp.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipsp.AD.before.p.no2015c,field_season!="KI2016a")
+# plot_bar(phy97.f.c.dipsp.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+# plot_bar(phy97.f.c.dipsp.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+
+phy97.f.c.dipall.AD.before <- merge_phyloseq(phy97.f.c.dipmat.AD.before,phy97.f.c.dipsp.AD.before)
+
+phy97.f.c.dipall.AD.before.p <- transform_sample_counts(phy97.f.c.dipall.AD.before, function(x) x/sum(x))
+phy97.f.c.dipall.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipall.AD.before.p,field_season!="KI2015c")
+phy97.f.c.dipall.AD.before.p.no2015c <- subset_samples(phy97.f.c.dipall.AD.before.p.no2015c,field_season!="KI2016a")
+plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+
+
+phy97.f.c.hydno.AD.before.p <- transform_sample_counts(phy97.f.c.hydno.AD.before, function(x) x/sum(x))
+phy97.f.c.hydno.AD.before.p.no2015c <- subset_samples(phy97.f.c.hydno.AD.before.p,field_season!="KI2015c")
+phy97.f.c.hydno.AD.before.p.no2015c <- subset_samples(phy97.f.c.hydno.AD.before.p.no2015c,field_season!="KI2016a")
+plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+
+jpeg(filename = "figures/Supplementary_Materials/platy_barplot_clade_outcome.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/platy_barplot_clade_dist.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+
+jpeg(filename = "figures/Supplementary_Materials/fpenta_barplot_clade_outcome.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/fpenta_barplot_clade_dist.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+
+jpeg(filename = "figures/Supplementary_Materials/dipall_barplot_clade_outcome.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/dipall_barplot_clade_dist.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+
+jpeg(filename = "figures/Supplementary_Materials/hydno_barplot_clade_outcome.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="clade")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/hydno_barplot_clade_dist.jpg",width = 10, height= 6, units = "in", res=300)
+plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="clade")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values=clade_cols)
+dev.off()
+
+
+jpeg(filename = "figures/Supplementary_Materials/hydno_barplot_its2type_outcome.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.hydno.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.hydno.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/hydno_barplot_its2type_dist.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.hydno.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.hydno.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.hydno.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/platy_barplot_its2type_outcome.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.platy.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.platy.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/platy_barplot_its2type_dist.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.platy.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.platy.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.platy.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
+
+
+jpeg(filename = "figures/Supplementary_Materials/fpenta_barplot_its2type_outcome.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.fpenta.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.fpenta.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/fpenta_barplot_its2type_dist.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.fpenta.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.fpenta.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.fpenta.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
+
+
+jpeg(filename = "figures/Supplementary_Materials/dipall_barplot_its2type_outcome.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.dipall.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.dipall.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("outcome",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
+
+jpeg(filename = "figures/Supplementary_Materials/dipall_barplot_its2type_dist.jpg",width = 10, height= 12, units = "in", res=300)
+colourCount = length(unique(data.frame(tax_table(phy97.f.c.dipall.AD.before.p.no2015c))$its2_type_profile))
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+p1 <- plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="its2_type_profile")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette(colourCount))
+
+colourCount2 = length(unique(data.frame(tax_table(phy97.f.c.dipall.AD.before.p.no2015c))$majority_its2_sequence))
+getPalette2 = colorRampPalette(brewer.pal(9, "Set3"))
+p2 <- plot_bar(phy97.f.c.dipall.AD.before.p.no2015c,fill="majority_its2_sequence")+facet_grid("disturbance_level",scales="free",space="free")+scale_fill_manual(values = getPalette2(colourCount2))
+
+grid.arrange(p1,p2,nrow=2)
+
+dev.off()
