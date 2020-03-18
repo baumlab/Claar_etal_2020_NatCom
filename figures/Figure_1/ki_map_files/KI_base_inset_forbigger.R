977c05417344b1ea6b2c24ef58f2047a35ac5f78
daniclaar <danielle.claar@gmail.com>
2020-03-18 15:10:13 -0700

    make supp map by region

--- figures/Figure_1/ki_map_files/KI_base_inset_forbigger.R
+++ figures/Figure_1/ki_map_files/KI_base_inset_forbigger.R
@@ -1,38 +1,18 @@
 ## Script for creating KI map inset
-#setwd("/Users/jpwrobinson/Documents/git_repos/maps")
- setwd("/Users/KristinaTietjen/Documents/Git_Hub/maps")
-# setwd("/Users/IMAC3/Documents/git-jpwrobinson/maps")
-#setwd("/Users/jamiemcdevitt-irwin/Documents/Git_Repos/maps")
-
 library(maptools)
 library(maps)
 library(scales)
 library(RColorBrewer)
-#install.packages("mapdata")
 library(mapdata)
-#install.packages("mapproj")
 library(mapproj)
-#install.packages("ggmap")
 library(ggmap)
 library(png)
-#install.packages("grImport")
 library(grImport)
  library(rgdal)
 library(scales)
-#install.packages("PBSmapping")
 library(PBSmapping)
 
-
-
-
-
-
-
-###complex inset
-# world_shp<-readShapePoly("/Users/jpwrobinson/Documents/git_repos/maps/shapes/ne_110m_land/ne_110m_land")
-# world_shp<-readShapePoly("/Users/IMAC3/Documents/git-jpwrobinson/maps/shapes/ne_110m_land/ne_110m_land")
- world_shp<-readShapePoly("/Users/KristinaTietjen/Documents/Git_Hub/maps/shapes/ne_110m_land/ne_110m_land")
-#world_shp <- readShapePoly("/Users/jamiemcdevitt-irwin/Documents/Git_Repos/maps/shapes/ne_110m_land/ne_110m_land")
+world_shp<-readShapePoly("figures/Figure_1/ki_map_files/shapes/ne_110m_land/ne_110m_land")
 
 plot(world_shp, xlim=c(-180,-80),ylim=c(-40,40), col=alpha("#B6A178", 0.3), bg="white",cex=0.2, lwd=0.2)
 box(lwd=1)
