f40f66d9a251e1f7baacfbcea532c50e7e0a86bd
daniclaar <danielle.claar@gmail.com>
2020-07-13 11:25:11 -0700

    cleanup

--- data/dep/test2/awk_no_blast_hits_KI_Platy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-awk '/No blast hit/' /Users/Danielle/Documents/Data_Analysis/KI_Platy/data/test2/KI14FSYM004_otus_out/KI14FSYM004_rep_set_tax_assignments.txt > /Users/Danielle/Documents/Data_Analysis/KI_Platy/data/test2/KI14FSYM004_otus_out/KI14FSYM004_no_hits.txt
-
--- data/dep/test2/iu-merge-pairs_KI_Platy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -u
-set -o pipefail
-
-iu-merge-pairs -o OR19_S307_L001-QUALITY_PASSED --enforce-Q30-check OR19_S307_L001-QUALITY_PASSED_merge_config
-iu-merge-pairs -o OR4_S333_L001-QUALITY_PASSED --enforce-Q30-check OR4_S333_L001-QUALITY_PASSED_merge_config
