dbfbb99652e95220c00fdeac63cad4fd1319629a
daniclaar <danielle.claar@gmail.com>
2020-07-13 12:00:23 -0700

    major cleanup

--- analyses/dep/coral99/merge_coral99.sh
+++
@@ -1,9 +0,0 @@
-merge-illumina-pairs -o KI_Platy_KI14FSYM099 --enforce-Q30-check config_KI_Platy_KI14FSYM099 --marker-gene-stringent --retain-only-overlap  --min-overlap-size 100 --compute-qual-dicts
-
-merge-illumina-pairs -o KI_Platy_KI15aFSYM103 --enforce-Q30-check config_KI_Platy_KI15aFSYM103 --marker-gene-stringent --retain-only-overlap  --min-overlap-size 100 --compute-qual-dicts
-
-merge-illumina-pairs -o KI_Platy_KI15bFSYM023 --enforce-Q30-check config_KI_Platy_KI15bFSYM023 --marker-gene-stringent --retain-only-overlap  --min-overlap-size 100 --compute-qual-dicts
-
-merge-illumina-pairs -o KI_Platy_KI15cFSYM018 --enforce-Q30-check config_KI_Platy_KI15cFSYM018 --marker-gene-stringent --retain-only-overlap  --min-overlap-size 100 --compute-qual-dicts
-
-merge-illumina-pairs -o KI_Platy_KI16aFSYM049 --enforce-Q30-check config_KI_Platy_KI16aFSYM049 --marker-gene-stringent --retain-only-overlap  --min-overlap-size 100 --compute-qual-dicts
