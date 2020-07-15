780b82c8b40588592534ff9572fdb5168dcf7cdf
daniclaar <danielle.claar@gmail.com>
2020-07-15 10:16:36 -0700

    cleanup

---
+++ figures/Extended_Data/Extended_Data_Figure_2/Fig_S2g_plot_dhws_platy.R
@@ -0,0 +1,44 @@
+# Load necessary packages
+library(ggplot2)
+library(gridExtra)
+
+# Load necessary data
+load("data/temperature/DHW_all.RData") # From https://github.com/daniclaar/KI_temperature_insitu_NOAA/blob/master/data/DHW_all.RData
+load("data/temperature/NOAA_DHW_5km.RData") # From https://github.com/daniclaar/KI_temperature_insitu_NOAA/blob/master/data/NOAA_DHW_5km.RData
+
+# Set x limits
+xlim1 <- as.POSIXct("2011/6/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                    tz="Pacific/Kiritimati")
+xlim2 <- as.POSIXct("2017/1/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                    tz="Pacific/Kiritimati")
+xlim3 <- as.POSIXct("2015/2/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                    tz="Pacific/Kiritimati")
+xlim4 <- as.POSIXct("2016/9/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                    tz="Pacific/Kiritimati")
+
+# Plot
+NOAA_DHW <- ggplot()+ 
+  theme_classic()+
+  geom_hline(yintercept = 4,linetype="dashed")+
+  geom_hline(yintercept = 8,linetype="dashed")+
+  geom_hline(yintercept = 12,linetype="dashed")+
+  geom_hline(yintercept = 16,linetype="dashed")+
+  geom_hline(yintercept = 20,linetype="dashed")+
+  geom_hline(yintercept = 24,linetype="dashed")+
+  geom_hline(yintercept = 28,linetype="dashed")+
+  geom_line(aes(x=date,y=dhw),color="#5F4690",data=dhw_vaskess)+
+  geom_line(aes(x=date,y=dhw),color="#1D6996",data=dhw_southlagoon)+
+  geom_line(aes(x=date,y=dhw),color="#0F8554",data=dhw_lagoonface)+
+  geom_line(aes(x=date,y=dhw),color="#EDAD08",data=dhw_northlagoon)+
+  geom_line(aes(x=date,y=dhw),color="#E17C05",data=dhw_northshore)+
+  geom_line(aes(x=date,y=dhw),color="#CC503E",data=dhw_BOW)+
+  scale_x_datetime(name=NULL, expand=c(0,0), limits=c(xlim3,xlim4),
+                   date_labels = "%b-%Y",date_breaks = "3 months")+
+  scale_y_continuous(name="DHW (ºC-week)",limits=c(0,25.6),expand=c(0,0), 
+                     breaks = c(0,4,8,12,16,20,24))
+
+# Make pdf
+pdf(file = "figures/Extended_Data/Extended_Data_Figure_2/KI_DHWs.pdf", 
+    width = 5.5, height = 4.5, useDingbats = FALSE)
+NOAA_DHW
+dev.off()
---
+++ figures/Extended_Data/Extended_Data_Figure_2/supp_map/Fig_S2a_KI_maps_platy_regions_supp.R
@@ -0,0 +1,62 @@
+# Plotting map showing regions
+
+### site data
+sites<-read.csv('figures/Figure_1/ki_map_files/ki_sites_updated2019.csv')
+
+### reordering levels for colouring plot
+levels(sites$region) # VB,SL,LF,NL,NS,BOW
+sites$region<-factor(sites$region, levels(factor(sites$region))[c(6,5,2,3,4,1)])
+
+# Set colors and map to factor levels
+region.cols<-c("#5F4690","#1D6996","#0F8554","#EDAD08","#E17C05","#CC503E")
+site.cols<-c("#5F4690","#1D6996","#0F8554","#EDAD08","#E17C05","#CC503E")
+region.cols<-as.data.frame(region.cols)
+region.cols$region<-levels(sites$region)
+sites$col<-region.cols$region.cols[match(sites$region, region.cols$region)]
+
+# Rename regions for plotting
+levels(sites$region) <- c("Vaskess Bay", "South Lagoon", "Mid Lagoon",
+                          "North Lagoon", "North Shore", "Bay of Wrecks")
+
+# Open pdf
+pdf(file="figures/Extended_Data/Extended_Data_Figure_2/supp_map/KI_map_platy_regions_inset_bigger.pdf", 
+    width = 7.5, height =7, useDingbats = FALSE)
+# Source the base map
+source("figures/Figure_1/ki_map_files/KI_base_B&W_bigger.R")
+# Add sites
+points(sites$lon, sites$lat, bg=alpha(sites$col,0.8), pch=21, cex=1.4)
+# Label sites
+with(sites, text(lon, lat, label=site.simple, cex=0.3))
+# Create legend
+legend(-157.29, 2.085, legend=levels(sites$region),
+       pt.bg=site.cols, pch=21, bty="n", pt.cex=1.4, cex=0.6)
+# Label areas
+text(-157.315, 1.88, "Bay of\nWrecks", cex = 0.45)
+text(-157.515, 1.825, "Vaskess\nBay", cex = 0.45)
+text(-157.455, 1.917, "Lagoon", cex = 0.45)
+# Set margins for inset
+par(mar=c(1.3,0.9,0.5,0.5))
+# Source inset script
+source("figures/Figure_1/ki_map_files/KI_base_inset_forbigger.R")
+dev.off() # close pdf
+
+# Open jpeg
+jpeg(filename = "figures/Extended_Data/Extended_Data_Figure_2/supp_map/KI_map_platy_regions_inset_bigger.jpg", width = 7.5, height =7, units="in", res=300)
+# Source the base map
+source("figures/Figure_1/ki_map_files/KI_base_B&W_bigger.R")
+# Add sites
+points(sites$lon, sites$lat, bg=alpha(sites$col,0.8), pch=21, cex=1.4) 
+# Label sites
+with(sites, text(lon, lat, label=site.simple, cex=0.3))
+# Create legend
+legend(-157.29, 2.085, legend=levels(sites$region), 
+       pt.bg=site.cols, pch=21, bty="n", pt.cex=1.4, cex=0.6)
+# Label areas
+text(-157.315, 1.88, "Bay of\nWrecks", cex = 0.45)
+text(-157.515, 1.825, "Vaskess\nBay", cex = 0.45)
+text(-157.455, 1.917, "Lagoon", cex = 0.45)
+# Set margins for inset
+par(mar=c(1.3,0.9,0.5,0.5))
+# Source inset script
+source("figures/Figure_1/ki_map_files/KI_base_inset_forbigger.R")
+dev.off() # close jpeg
