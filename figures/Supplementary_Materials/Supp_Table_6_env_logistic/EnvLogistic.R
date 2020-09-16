a18559b8a761d49f69ee18c7e1675157e2649893
Sam Starko <samstarko@Sams-MBP.hitronhub.home>
2020-09-16 16:54:58 -0700

    Updating environmental logistic regressions to fix wave exposure issue

--- figures/Supplementary_Materials/Supp_Table_6_env_logistic/EnvLogistic.R
+++ figures/Supplementary_Materials/Supp_Table_6_env_logistic/EnvLogistic.R
@@ -3,10 +3,17 @@
 
 # Load necessary libraries
 library(readxl)
+library(tidyverse)
+library(arm)
 
 # Load necessary data
 env<-read_excel("data/environmental_parameters/KI_env_all.xlsx")
 logist<-read_excel("data/Logistic_regression_data/LogisticData.xlsx")
+
+#Run the following code only to remove sites that did not have reliable wave data
+#For all other analyses, do not run this chunk
+#env2 <- filter(env, wave_wind_fetch_sat == "0")
+#env<-env2
 
 # Rename column
 colnames(logist)[3]<-"site"
@@ -31,10 +38,15 @@ env_avg<-env %>%
             NPPmax=mean(npp_max_sat, na.rm=TRUE),
             wave=mean(wave_mean_sat, na.rm=TRUE))
 dat<-left_join(env_avg, logist, by = "site")
+dat$ProportionD_before<-as.numeric(dat$ProportionD_before)
+
 
 # Subset by coral species
 dat1<-subset(dat,Coral_Species=="Platygyra")
 dat2<-subset(dat,Coral_Species=="Favites")
+
+#Filter to subset for wave exposure
+
 
 # Run glms
 bayesglm(ProportionD_before ~ Disturbance_sqrt, data=dat1, 
@@ -47,12 +59,14 @@ bayesglm(ProportionD_before ~ pH+Disturbance_sqrt, data=dat1,
          family="quasibinomial") %>% summary()
 bayesglm(ProportionD_before ~ DO_sat+Disturbance_sqrt, 
          data=dat1, family="quasibinomial") %>% summary()
-bayesglm(ProportionD_before ~ wave+Disturbance_sqrt, data=dat1, 
-         family="quasibinomial") %>% summary()
 bayesglm(ProportionD_before ~ NPPmean+Disturbance_sqrt, data=dat1, 
          family="quasibinomial") %>% summary()
 bayesglm(ProportionD_before ~ NPPmax+Disturbance_sqrt, data=dat1, 
          family="quasibinomial") %>% summary()
+
+#Only run this analysis after subsetting above (see third chunk of code)
+bayesglm(ProportionD_before ~ wave+Disturbance_sqrt, data=dat1, 
+         family="quasibinomial") %>% summary() 
 
 # Plot
 ggplot(data=dat1, aes(y=ProportionD_before, x=NPPmax) ) +
@@ -77,8 +91,6 @@ bayesglm(ProportionD_before ~ pH+Disturbance_sqrt, data=dat2,
          family="quasibinomial") %>% summary()
 bayesglm(ProportionD_before ~ DO_sat+Disturbance_sqrt, data=dat2, 
          family="quasibinomial") %>% summary()
-bayesglm(ProportionD_before ~ wave+Disturbance_sqrt, data=dat2, 
-         family="quasibinomial") %>% summary()
 bayesglm(ProportionD_before ~ NPPmean+Disturbance_sqrt, data=dat2, 
          family="quasibinomial") %>% summary()
 bayesglm(ProportionD_before ~ NPPmax+Disturbance_sqrt, data=dat2, 
@@ -86,3 +98,6 @@ bayesglm(ProportionD_before ~ NPPmax+Disturbance_sqrt, data=dat2,
 bayesglm(ProportionD_before ~ NPPmax+Disturbance_sqrt, data=dat2, 
          family="quasibinomial") %>% summary() 
 
+#Only run this analysis after subsetting above (see third chunk of code)
+bayesglm(ProportionD_before ~ wave+Disturbance_sqrt, data=dat2, 
+         family="quasibinomial") %>% summary()
